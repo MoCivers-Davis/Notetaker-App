af36bc48f7d092d7cf298f41a4bc52e61802250d
Mo Civ <mocivers@comcast.net>
2020-05-15 21:03:56 -0400

    tryint to update homepage

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const outputPath = path.join(OUTPUT_DIR, "db.json");
 
 ////////////////////HTML///////////////////
 app.get("/", function (req, res) {/////HOME PAGE
-    res.sendFile(path.join(__dirname, "public/index.html"));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.get("/notes", function (req, res) {////Notes Homepage
