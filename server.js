24186edc5bd6a6565e7e95841ecd185c66c32943
Mo Civ <mocivers@comcast.net>
2020-05-15 21:11:57 -0400

    Some stuff

--- server.js
+++ server.js
@@ -20,11 +20,11 @@ const outputPath = path.join(OUTPUT_DIR, "db.json");
 
 ////////////////////HTML///////////////////
 app.get("/", function (req, res) {/////HOME PAGE
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.get("/notes", function (req, res) {////Notes Homepage
-    res.sendFile(path.join(__dirname, "public/notes.html"));
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 })
 
 var y = 1
